d9329cceb46ae2ff8b8a11b96ffb9ccba70077ed
Ricardo Yanez <riyaez@gmail.com>
2020-10-12 19:24:25 -0400

    Fin labo

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburger = document.querySelector('.hamburger');
+    var menuMoblie = document.querySelector(".nav-primary-mobile");
+
+    hamburger.addEventListener('click', openMenu);
+
+
+
+
+    function openMenu(evt){
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if(cible.classList.contains("open")){
+            cible.classList.remove("open");
+            menuMoblie.classList.remove("open");
+        }else {
+            cible.classList.add("open");
+            menuMoblie.classList.add("open");
+        }
+
+    }
+
 });
